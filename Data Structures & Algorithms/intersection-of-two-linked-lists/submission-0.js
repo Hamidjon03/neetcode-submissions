61674972c6c9edd5f200ffce50661608bcd6b26a
Hamidjon03 <74987338+Hamidjon03@users.noreply.github.com>
2026-04-22 12:42:23 +0500

    Add: intersection-of-two-linked-lists - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-0.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} headA
+     * @param {ListNode} headB
+     * @return {ListNode}
+     */
+    getIntersectionNode(headA, headB) {
+        let lA = headA;
+        let lB = headB;
+
+        while(lA !== lB){
+            lA = lA ? lA.next : headB;
+            lB = lB ? lB.next : headA;
+            if(lA === lB) return lA
+        }
+
+        return null
+    }
+}
