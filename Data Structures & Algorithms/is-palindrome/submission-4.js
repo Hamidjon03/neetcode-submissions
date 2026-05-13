539a37f359fe3ecc7ac3050d72ac82f41763c90c
Hamidjon03 <74987338+Hamidjon03@users.noreply.github.com>
2026-05-13 15:47:41 +0500

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+     isPalindrome(str){
+        let left = 0;
+        let right = str.length - 1;
+
+        while(left < right){
+
+            if(!this.isAplphaNumeric(str[left])){
+                left++;
+                continue;
+            }
+            if(!this.isAplphaNumeric(str[right])){
+                right--;
+                continue;
+            }
+
+            if(str[left].toLowerCase() !== str[right].toLowerCase()) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    isAplphaNumeric(char){
+        return (
+            (char >= "a" && char <= "z") ||
+            (char >= "A" && char <= "Z") ||
+            (char >= "0" && char <= "9")
+        )
+    }
+}
