fc592863e2e326b48d1fadecb761f32c1cf06e67
Hamidjon03 <74987338+Hamidjon03@users.noreply.github.com>
2026-05-13 12:25:59 +0500

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+
+        for(let str of strs){
+            let length = str.length;
+            result += length + "#" + str;
+        }
+
+        return result;
+    }
+
+
+     // "5#Hello5#World
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result = [];
+        let i = 0;
+        while(i < str.length){
+            let j = i;
+            while(str[j] !== "#"){
+                j++;
+            }
+
+            let length = parseInt(str.substring(i, j));
+            let start = j + 1;
+            let end = start + length;
+            result.push(str.substring(start, end));
+
+            i = end;
+        }
+        return result;
+    }
+    
+
+}
