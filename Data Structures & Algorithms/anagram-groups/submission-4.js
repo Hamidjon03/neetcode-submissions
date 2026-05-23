af95d76829066e71152d0cee366c7045db9445e6
Hamidjon03 <74987338+Hamidjon03@users.noreply.github.com>
2026-05-23 12:47:10 +0500

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+
+    groupAnagrams(strs) {
+        let res = {};
+
+        for(const str of strs){
+            let count = new Array(26).fill(0);
+            for(const char of str){
+                count[char.charCodeAt(0) - "a".charCodeAt(0)]++
+            }
+            const key = count.join("#");
+            if(!res[key]){
+                res[key] = [];
+            }
+            res[key].push(str);
+        }
+
+        return Object.values(res);
+    }
+}
