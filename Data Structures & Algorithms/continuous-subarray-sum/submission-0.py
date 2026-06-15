0409394f818bd89c2ccca841abb6b286bd05103a
Hamidjon03 <74987338+Hamidjon03@users.noreply.github.com>
2026-06-15 07:50:13 +0500

    Add: continuous-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        remainder_map = {0: -1}
+        current_sum = 0
+
+        for i , num in enumerate(nums):
+            current_sum += num
+            remainder = current_sum % k
+
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] >= 2:
+                    return True
+                continue
+            
+            remainder_map[remainder] = i
+
+        return False
